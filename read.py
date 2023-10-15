cb0e8ed11f9cf670eb99d158648901c153814449
2003achuajays <142298504+2003achuajays@users.noreply.github.com>
2023-10-15 21:59:32 +0530

    4 commit

--- read.py
+++ read.py
@@ -1,26 +1,37 @@
 import cv2 as cv
 
+def recale(frame , scale = 0.75):
+    w = int(frame.shape[1] * scale)
+    h = int(frame.shape[0] * scale)
 
+    dime = (w,h)
+
+
+    return cv.resize(frame , dime , interpolation=cv.INTER_AREA)
 # reading photo
 
 # img = cv.imread('photo/1.jpg')
+# imgr  = recale(img,0.2)
 
 # cv.imshow('cat' , img)
+# cv.imshow('1 1 ' , imgr)
 
 # reading video
 
 
-capture = cv.VideoCapture('video/1.mkv')
+# capture = cv.VideoCapture(0)
 
-while True:
-    isTrue , frame = capture.read()
-    cv.imshow('video', frame)
+# while True:
+#     isTrue , frame = capture.read()
+#     framere = recale(frame , 2)
+#     cv.imshow('video', frame)
+#     cv.imshow('video frame', framere)
 
-    if cv.waitKey(20) &  0XFF == ord('d'):
-        break
 
-capture.release()
-cv.destroyAllWindows()
+#     if cv.waitKey(20) &  0XFF == ord('d'):
+#         break
 
+# capture.release()
+# cv.destroyAllWindows()
 
 cv.waitKey(0)
